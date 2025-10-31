0bdf7a3ddf12b73dd42a521331be54d84848721f
Duru Favour <favourduru24@gmail.com>
2025-10-30 17:51:52 -0700

    new code update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+     typescript: {
+      ignoreBuildErrors: true
+     },
 };
 
 export default nextConfig;
